28f5cff3d2fbb5af11615c24f81565d8aee01490
Justin James <justin.james@yale.edu>
2020-05-15 23:05:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,15 +5,14 @@ def key_for_min_value(name_hash)
   if name_hash.empty?
     return nil
   end
-
   min_value = -1 / 0.0
-  name_hash.each do |key, value|
-    if value < min_value 
+  name_hash.each do |(key, value)|
+    if (value < min_value) 
       min_value = value
     end 
   end
   name_hash.each do |(key, value)|
-    if value == min_value
+    if (value == min_value)
       return key
     end
   end
